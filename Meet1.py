3f562d051a18052c3c4d5ea8b07d7e00406aecc0
Kezia Ardia Sasaki <keziaardia03@gmail.com>
2025-04-11 18:37:20 +0800

    Add files via upload

---
+++ Meet1.py
@@ -0,0 +1,25 @@
+# perbandingan kemungkinan nilai = true/false  (bolean)
+# 1 > 2 =
+# <, >, >=, <=, !=, not
+
+nim = 24241169
+a = 24
+b = 69
+
+hasil = a < b
+print(hasil)
+
+hasil = a > b
+print(hasil)
+
+hasil = a >= b
+print(hasil)
+
+hasil = a <= b
+print(hasil)
+
+hasil = a != b
+print(hasil)
+
+("hasil = a not b")
+print(hasil)
