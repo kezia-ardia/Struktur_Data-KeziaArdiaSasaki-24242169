e7750f1c9d34ac563fd6112c26557e3146450b76
Kezia Ardia Sasaki <keziaardia03@gmail.com>
2025-06-14 08:01:25 +0800

    Add files via upload

---
+++ Meet8.py
@@ -0,0 +1,10 @@
+#terdapat variabel "number" yang isinya (1234567890) buatlah aplikasi untuk
+#menampilkan data terakhir
+#menampilkan data pertama
+#menampilkan data ke3-ke6
+
+number = "1234567890"
+
+print("data terakhir:", number[-1])
+print("data pertama:", number[0])
+print("data ke 3 - ke 6:", number[2:6])
